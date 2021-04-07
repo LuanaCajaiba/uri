604d25d6442729636b2329eef83567882d77ed32
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-07 03:28:45 -0300

    Início

--- iniciante/1000.cpp
+++ iniciante/1000.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
+#include <iostream> //#incluse - diretiva de pré-processador usada para incluir arquivos no programa
+                    //iostream - arquivo que está sendo incluido
+                    //o arquivo iostream permite usar o "cout" que é responsável por imprimir a saída na tela
  
 using namespace std;
  
-int main(void) {
+int main(void) { //início da função
  
  std::cout << "Hello World!";
  
